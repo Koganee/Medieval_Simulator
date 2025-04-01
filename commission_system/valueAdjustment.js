7db02c604f725e4d7da7f20d012c441c0520e098
Aaron Murphy <126209301+Koganee@users.noreply.github.com>
2025-04-01 23:08:38 +0100

    Update valueAdjustment.js

--- commission_system/valueAdjustment.js
+++ commission_system/valueAdjustment.js
@@ -10,5 +10,15 @@ function valueAdjustment(wealth)
 
 function stockValue()
 {
-    
+    for(let i = 0; i < window.arrayArt.length; i++)
+    {
+        if(window.arrayArt[i] != null)
+        {
+            if(window.arrayArtValue[i] < 60)
+            {
+                window.arrayArtValue[i] = window.arrayArtValue[i] + 10;
+            }
+        }
+        console.log("Art value at index", i, ":", window.arrayArtValue[i]); // Log the art value for debugging
+    }
 }
